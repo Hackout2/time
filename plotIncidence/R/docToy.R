50ad21e1282e57986e6d8f066bfeff6d3ea5a8d2
thibautjombart <thibautjombart@gmail.com>
2015-02-11 16:00:49 +0000

    adding doc for toy dataset

---
+++ plotIncidence/R/docToy.R
@@ -0,0 +1,14 @@
+#' Toy outbreak dataset
+#'
+#' This is a simplified version of Michael Hohle's dataset Hagelloch
+#'
+#' \itemize{
+#'   \item dateOfOnset a vector of dates of symptom onset in Date format
+#'   \item gender the gender of the patient
+#'   \item complication an indication of possible clinical complications
+#' }
+#'
+#' @format A data frame with 187 rows and 3 variables
+#' @source Michael Hohle's dataset Hagelloch
+#' @name toy
+NULL
