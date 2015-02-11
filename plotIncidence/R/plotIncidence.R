4ba1223c215b1d56eb38d36b2f85117ac4e8f863
Thibaut Jombart <thibautjombart@gmail.com>
2015-02-11 14:27:05 +0000

    adding new package for incidence plotting

---
+++ plotIncidence/R/plotIncidence.R
@@ -0,0 +1,21 @@
+#'Plot incidence time series
+#'
+#'This function provides advanced plotting facilities for incidence time series using ggplot2
+#' @param x a data.frame containing the data to be plotted.
+## @param  y <what param does>
+#' @export
+#' @examples
+#'
+#' ## simulate a time series
+#' x <- as.Date("2015-02-12") + sample(1:80, replace=TRUE, size=500)
+#'
+#'
+
+###################
+## plotIncidence ##
+###################
+plotIncidence <- function(x, bin=7, split.by=NULL, fill.by=NULL, shade.by=NULL) {
+
+
+} # end plotIncidence
+
