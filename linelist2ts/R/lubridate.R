08703ff7219aff6bfbe8f78b071d5847255c53e5
hoehleatsu <hoehle@lillagubben>
2015-02-12 17:04:07 +0000

    forgot to add some files to git

---
+++ linelist2ts/R/lubridate.R
@@ -0,0 +1,22 @@
+#' Version of the wday function from lubridate, where the monday is the 1.
+#'
+#' The function returns a vector of integer, where mondays are 1 and sundays are 7.
+#'
+#'@note Function could have been written with pure strptime commands in case
+#'one does not want the lubridate dependencies.
+#'
+#' @param date - vector of dates
+#' @return vector of numeric with the weekdays (mon=1, sun=7)
+#' @export
+wdaymon <- function(date) { lubridate::wday(date - 1) }
+
+#' Function to get the monday of the corresponding week a date belongs to.
+#'
+#' @note Function could have been written with pure strptime commands in case
+#' one does not want the lubridate dependencies. This is the function Sebastian
+#' always wanted, sorry it's not in a standalone package.
+#'
+#' @param date vector of dates
+#' @return vector of dates
+#' @export
+monday  <- function(date) { date - wdaymon(date) + 1}
