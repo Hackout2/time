8bfe23894ce60fdf3a53ceeb1416fa082d39565d
perrinepelosse <perrine.pelosse@gmail.com>
2015-02-12 17:01:33 +0000

    added documentation simulated dataset

--- plotIncidence/R/docZombie.R
+++ plotIncidence/R/docZombie.R
@@ -14,19 +14,17 @@
 NULL
 
 
-
-
-#' #' #' Toy outbreak dataset
+#' #' #' Zombie outbreak dataset
 #'
-#' This is a simplified version of Michael Hohle's dataset Hagelloch
+#' This is a dataset simulating an outbreak with geo-localisation of the cases (6 localities)  
 #'
 #' \itemize{
-#'   \item dateOfOnset a vector of dates of symptom onset in Date format
+#'   \item x.coord a vector of x coordinates
+#'   \item y.coord a vector of y coordinates
+#'   \item date a vector of dates of recorded cases in Date format
 #'   \item gender the gender of the patient
-#'   \item complication an indication of possible clinical complications
 #' }
 #'
-#' @format A data frame with 187 rows and 3 variables
-#' @source Michael Hohle's dataset Hagelloch
-#' @name toy
+#' @format A data frame with 154013 rows and 4 variables
+#' @name zombie_outbreak
 NULL
