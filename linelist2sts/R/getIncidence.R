03d169e85610fbd9e5278fab5ee4b3f9ec98ee39
hoehleatsu <hoehle@lillagubben>
2015-02-12 00:13:00 +0000

    conversion from list of lists to multivariate xts now works

--- linelist2sts/R/getIncidence.R
+++ linelist2sts/R/getIncidence.R
@@ -5,6 +5,11 @@
 #' 
 #' @author Michael Höhle
 #' 
+#' 
+
+#' Generic 
+setGeneric("get.incidence2", function(x, ...) standardGeneric("get.incidence2"))
+
 ####################
 ## obkData method ##
 ####################
@@ -12,7 +17,7 @@
 ## based on 'dates' associated to a given field
 ## 'values' are optional and can be used to subset the retained 'dates'
 ## (e.g. define what a positive case is)
-setMethod("get.incidence", "obkData", function(x, data, where=NULL, val.min=NULL, val.max=NULL, val.kept=NULL, regexp=NULL,
+setMethod("get.incidence2", "obkData", function(x, data, where=NULL, val.min=NULL, val.max=NULL, val.kept=NULL, regexp=NULL,
                                                from=NULL, to=NULL, interval=1, add.zero=TRUE, doBy=NULL, ...){
   ## HANDLE ARGUMENTS ##
   if(is.null(val.min)) val.min <- -Inf
@@ -76,59 +81,91 @@ setMethod("get.incidence", "obkData", function(x, data, where=NULL, val.min=NULL
     dates <- dates[toKeep]
   }
   
-  ##If there is nothing.
+  ##If there are no dates we are done.
   if(length(dates)==0) return(NULL)
   
-  ##hoehle -- 
+  ##Prepare the return list
   res <- list()
+  
+  #If there is no from-to specification make 
+  #sure it's not data subset dependend, but is the 
+  #same for each subset.
+  if (is.null(from) & is.null(to)) {
+    from <- min(dates)
+    to <- max(dates)
+  }
+  
+  ##Loop over all variables in doBy 
   if (!is.null(doBy)) {
-    for (i in 1:length(doBy)) {
-     # browser()
+    for (i in seq_len(length(doBy))) {
+   #   browser()
       theData <- get.data(x, data=doBy[[i]], showSource=TRUE)
       
-      if (is.factor(theData[,doBy[[i]]])) {
-        res[[doBy[i]]] <- tapply(dates, INDEX=theData[,doBy[[i]]], FUN=get.incidence, from=from, to=to, interval=interval, add.zero=add.zero)        
-      } else {
-        stop("The variable ",doBy[[i]]," is not a factor.")
-      }
+      if (is.null(theData)) stop(paste0("Data for ",doBy[[i]]," cannot be found in this obkData object."))      
+      if (!is.factor(theData[,doBy[[i]]])) stop("The variable ",doBy[[i]]," is not a factor.")
+       
+      res[[doBy[i]]] <- tapply(dates, INDEX=theData[,doBy[[i]]], FUN=get.incidence, from=from, to=to, interval=interval, add.zero=add.zero,simplify=FALSE)        
     } 
   } else {
     res <- list(get.incidence(dates, from=from, to=to, interval=interval, add.zero=add.zero))
   }
   
-  ## CALL THE DATE PROCEDURE ##
-  #if(length(dates)==0) return(NULL)
-  #out <- get.incidence(dates, from=from, to=to,
-  #                     interval=interval, add.zero=add.zero)
-  
   ## RETURN OUTPUT ##
   return(res)
 }) # end obkData method
 
-inc2xts <- function(inci) {
-  xts.list <- lapply(inci, function(list) lapply(list, function(df) {
-    print(dim(df))
-    with(df,  as.xts(incidence, order.by=date))
-  }))
-  #lapply(xts.list, merge). This is currently not working!
-  xts <- merge(xts.list[["SEX"]]$male, xts.list[["SEX"]]$female, fill=0)
+#' Helper function to format a get.incidence list of data.frames
+#' to a multivariate xts object
+#' 
+#' @param incList List of lists containing the data.frames from get.incidence2
+#' @return An xts object corresponding to the flattened incList
+#' @export
+inc2xts <- function(incList) {
+  #Convert each entry of incList from data.frame to xts. It's a list of xts obj
+  xtsList <- lapply(incList, function(list) {
+    lapply(list, function(df) {
+      with(df,  as.xts(incidence, order.by=date))
+    })
+  })
+  
+  #Code looping over all xts entries and merging them. data.table or plyr
+  #might do this better?
+  xts <- Reduce(cbind,lapply(xtsList, function(list) Reduce(cbind, list)))
+  
+  #Manual way of getting pretty (?) column names
+  lvl1 <- names(xtsList)
+  lvl2 <- lapply(xtsList, names)
+  mynames <- paste(rep(lvl1,times=sapply(lvl2,length)), do.call(c,lvl2),sep="-") 
+  dimnames(xts)[[2]] <- mynames
+  
+  #Is there a better way?!?!
+  
+  #Sanity checks
+  #all(xtsList[["SEX"]]$male == xts[,"SEX-male"])
+  #all(xtsList[["SEX"]]$female == xts[,"SEX-female"])
+  #all(xtsList[["AGEGRP"]][[1]] == xts[,"AGEGRP-(0,5]"])
+  
+  #xts <- Reduce(cbind, Reduce(cbind, xtsList))
+  #do.call(cbind, xtsList)
+  #data.table::rbindlist(xtsList)
+  
   return(xts)
 }
 
-doIt <- function() {
+sandboxIt <- function() {
   source("getIncidence.R")
-  hagelloch.obk@individuals$AGEGRP <- cut(hagelloch.obk@individuals$AGE, breaks=c(0,10,20,Inf))
-  inci <- get.incidence(hagelloch.obk, "timeERU", doBy=c("SEX","AGEGRP"), add.zero=FALSE)
-  length(inci)
-  str(inci)
+  
+  #Add extra column
+  hagelloch.obk@individuals$AGEGRP <- cut(hagelloch.obk@individuals$AGE, breaks=c(0,5,10,Inf))
+  
+  
+  inc <- get.incidence2(hagelloch.obk, "timeERU", doBy=c("SEX","CL"), add.zero=FALSE)
   
   #Show the time series.
-  plot(inc2xts(inci))  
-  plot(as.zoo(inc2xts(inci)), screens=c(1,2))
+  plot(inc2xts(inc))  
+  plot(as.zoo(inc2xts(inc)),plot.type='multiple')
+  plot(as.zoo(inc2xts(inc)), screens=1,col=c("magenta","steelblue"),lwd=3,type="h",cex.axis=0.8)
+
   plot(as.zoo(inc2xts(inci)), plot.type='multiple')
-  
-  
-  foo <- dygraph(inc2xts(inci), main = "Hagelloch") %>% 
-    dyRangeSelector(dateWindow = range(epoch(sts)))
-  foo  
+
 }
--- linelist2sts/R/interactive.R
+++ linelist2sts/R/interactive.R
@@ -4,13 +4,18 @@
 #' 
 library("dygraphs")
 library("xts")
+library("RColorBrewer")
+library("OutbreakTools")
+library("scales")
+library("ggplot2")
+
 #Overwrite getIncidence method for obkData such that it handles
 #new doBy argument. Could also use Thibaut's ggplot code, but
 #the advantage is that we get the time series directly (easily convertible to, e.g., xts)
 source("getIncidence.R")
 
 #Load example data
-load("data/hagelloch.RData")
+load("../data/hagelloch.RData")
 
 #Create extra factor variable for AGEGRPS
 hagelloch.obk@individuals$AGEGRP <- cut(hagelloch.obk@individuals$AGE, breaks=c(0,5,10,Inf))
@@ -20,19 +25,31 @@ hagelloch.obk@individuals$AGEGRP <- cut(hagelloch.obk@individuals$AGE, breaks=c(
 #inci <- get.incidence(hagelloch.obk, "timeERU", add.zero=FALSE)
 #Todo: Aggregate like a boss (using formula interface) as follows:
 #
-#           get.incidence(hagelloch.obk, timeERU ~ SEX)
+#           get.incidence2(hagelloch.obk, timeERU ~ SEX)
 #
-inci <- get.incidence(hagelloch.obk, "timeERU", doBy=c("SEX","AGEGRP"), add.zero=FALSE)
+#inc <- get.incidence(hagelloch.obk, "timeERU", doBy=c("SEX","AGEGRP"), add.zero=FALSE)
+inc <- get.incidence2(hagelloch.obk, "timeERU", doBy=c("SEX","CL"), add.zero=FALSE)
 
-#Convert to xts object
-#sts.xts <- xts( x=inci[[1]]$incidence, order.by=inci[[1]]$date, frequency=365)
-sts.xts <- inc2xts(inci)
-plot(as.zoo(sts.xts), screens=c(1,2))
+#Convert incList to xts object and plot
+sts.xts <- inc2xts(inc)
+pal <- brewer.pal(n=ncol(sts.xts),"Set3")
+plot(as.zoo(sts.xts), plot.type="single",col=pal,lwd=3,xlab="Onset of rash",ylab="No. individuals")
+grid(ny=NULL,nx=NA,col="darkgray")
+legend(x="topleft",colnames(sts.xts),col=pal,lwd=3)
+
+##########ggplot2 like plots from zoo objects ############
+#(see http://stackoverflow.com/questions/13848218/drawing-a-multiline-graph-with-ggplot2-from-a-zoo-object)
+p <- autoplot(sts.xts, facet = NULL)
+p
+p + aes(linetype = NULL)
+p + scale_x_date(labels = date_format("%d-%b"), xlab("Onset of rash")) +  scale_y_continuous(ylab("No. individuals"))
+
+###### Interactive graphics ######################
 
 #You can click and drag to zoom. Double-clicking will zoom you back out. Shift-drag will pan
 #See also: http://dygraphs.com/
 foo <- dygraph(sts.xts, main = "Hagelloch") %>% 
-  dyRangeSelector(dateWindow = range(epoch(sts)))
+  dyRangeSelector(dateWindow = range(index(sts.xts)))
 
 #Add some clickCallback handler - note that the alert function is javascript
 foo$x$attrs$clickCallback = htmlwidgets::JS('function(e,x,pts) { alert(JSON.stringify(pts))}')
